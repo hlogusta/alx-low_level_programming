6642abd3d9cc0b2abaf7cddbd975c76ab170a364
Stacey <hlogusta@gmail.com>
2023-03-01 13:51:27 -0800

    chat

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Stacey G Hlophe
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
