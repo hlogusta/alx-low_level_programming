9630db4970e23fc1beab12a830fc59537a437a38
Stacey <hlogusta@gmail.com>
2023-03-01 06:13:09 -0800

    lower

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,15 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Stacey G Hlophe
+ */
+
 #include <stdio.h>
+
+/**
+ * main-Prints the alphabet in lower case
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	char lower;
