fd099d35236c1bb67224a6d7dc51ff14ec0770d0
Stacey <hlogusta@gmail.com>
2023-03-01 06:30:35 -0800

    numve

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Stacey G Hlophe
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0.
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
