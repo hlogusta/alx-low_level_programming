204ec54df0086963f9df2e82466268b04ad04399
Stacey <hlogusta@gmail.com>
2023-03-02 10:36:01 -0800

    ki

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-mul.c
+ * Auth: Stacey G Hlophe
+ */
+
+#include "main.h"
+
+/**
+ * int mul - multiplies two integers.
+ * @a: the number to be multiplied.
+ * @b: the number to be multiplied.
+ *
+ * Return: the product of multiplication.
+ */
+int mul(int a, int b)
+{
+	return(a*b);
+}
