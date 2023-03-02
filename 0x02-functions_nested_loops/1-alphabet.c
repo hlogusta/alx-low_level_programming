08a83eebbcf254ed3685ba0ae60fc83616226c15
Stacey <hlogusta@gmail.com>
2023-03-02 08:29:43 -0800

    pt

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,12 @@
  * Auth: Stacey G Hlophe
  */
 
-#include <stdio.h>
 #include "main.h"
 
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
