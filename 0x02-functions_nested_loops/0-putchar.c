f614506ea9a5bd681a26e210bc40c335c0203d93
Stacey <hlogusta@gmail.com>
2023-03-02 02:06:20 -0800

    lo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,24 @@
 /*
  * File: 0-putchar.c
  * Auth: Stacey G Hlophe
- *
  */
 
-#include <stdio.h>
 #include "main.h"
 /**
  * main - Prints _putchar.
  *
  * Return: Always 0.
  */
-int main(void)
+int main(void);
 {
-	printf("_putchar\n");
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
