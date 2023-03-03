f1549ec927d01636db0bf4f477c30045821fe572
Stacey <hlogusta@gmail.com>
2023-03-03 08:49:22 -0800

    kk

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-main.c
+ * Auth: Stacey G Hlophe
+ */
+
+#include  "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
