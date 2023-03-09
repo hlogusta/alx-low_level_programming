0ad244f9dbb1ae90d178573d220e0dde7f13ee27
Stacey <hlogusta@gmail.com>
2023-03-09 04:36:12 -0800

    kk

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /*
- * _puts_recursion - prints a string.
- * @s - String
+ * _puts_recursion - Prints a string.
+ * @s - String to be printed.
  */
 
 void _puts_recursion(char *s)
@@ -12,5 +12,8 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+	{
 	_putchar('\n');
+	}
 }
