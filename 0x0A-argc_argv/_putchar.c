80dec61e2040d917a3630dcf2871610c9ac13b2a
Stacey <hlogusta@gmail.com>
2023-05-14 14:41:58 -0700

    a program that prints its name

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stout.
+ * @c: printed character.
+ * Return: On success 1.
+ * On error -1 is returned.
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
