e6bab7e90e02c45fd80084d710f9c8ec7b83b42b
Stacey <hlogusta@gmail.com>
2023-05-16 08:07:43 -0700

    Static library

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stout.
@@ -9,5 +9,5 @@
  */
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
